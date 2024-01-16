bbdc3d091045adb949199c58127b06bbe1276642
Xepobopa <ismartsdn4477@gmail.com>
2024-01-16 18:03:12 +0200

    Update to new Calculator version (0.0.3). Old code saved in initSocket.js.backup

--- util/initSocket.js
+++ util/initSocket.js
@@ -19,30 +19,36 @@ export default function initSocket(socket) {
             console.log(id, 'connected')
             socket.emit('init', { id })
         })
-        .on('request', (data) => {
-            emit(data.to, 'request', { from: id })
+        .on('connection', (data) => {
+            emit(data.to, 'connection', {...data, from: id});
         })
-        .on('encryptionPayload', (data) => {
-            emit(data.to, 'encryptionPayload', {...data, from: id })
-        })
-        .on('call', (data) => {
-            emit(data.to, 'call', { ...data, from: id })
-        })
-        .on('end', (data) => {
-            emit(data.to, 'end')
-        })
-        .on('voiceCallStart', (data) => {
-            emit(data.to, 'voiceCallStart', {...data, from: id})
-        })
-        .on('voiceCallReject', (data) => {
-            emit(data.to, 'voiceCallReject', {...data, from: id})
-        })
-        .on('voiceCallSuccess', (data) => {
-            emit(data.to, 'voiceCallSuccess', {...data, from: id})
-        })
-        .on('voiceCallEnd', (data) => {
-            emit(data.to, 'voiceCallEnd', {...data, from: id})
-        })
+        // .on('request', (data) => {
+        //     emit(data.to, 'request', { from: id })
+        // })
+        // .on('encryptionPayload', (data) => {
+        //     emit(data.to, 'encryptionPayload', {...data, from: id })
+        // })
+        // .on('call', (data) => {
+        //     emit(data.to, 'call', { ...data, from: id })
+        // })
+        // .on('end', (data) => {
+        //     emit(data.to, 'end')
+        // })
+        // .on('send', (data) => {
+        //     emit(data.to, 'send', { ...data, from: id });
+        // })
+        // .on('voiceCallStart', (data) => {
+        //     emit(data.to, 'voiceCallStart', {...data, from: id})
+        // })
+        // .on('voiceCallReject', (data) => {
+        //     emit(data.to, 'voiceCallReject', {...data, from: id})
+        // })
+        // .on('voiceCallSuccess', (data) => {
+        //     emit(data.to, 'voiceCallSuccess', {...data, from: id})
+        // })
+        // .on('voiceCallEnd', (data) => {
+        //     emit(data.to, 'voiceCallEnd', {...data, from: id})
+        // })
         .on('disconnect', () => {
             delete users[id]
             console.log(id, 'disconnected')
